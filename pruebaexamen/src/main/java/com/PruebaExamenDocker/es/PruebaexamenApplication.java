36991c1ff4bad17c5167d2476570a77f39ed0dad
Pedro Lemus <pedrolemuz@gmail.com>
2025-03-14 19:50:24 +0100

    Primer Commit

---
+++ pruebaexamen/src/main/java/com/PruebaExamenDocker/es/PruebaexamenApplication.java
@@ -0,0 +1,13 @@
+package com.PruebaExamenDocker.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaexamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaexamenApplication.class, args);
+	}
+
+}
---
+++ pruebaexamen/src/test/java/com/PruebaExamenDocker/es/PruebaexamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PruebaExamenDocker.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaexamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
